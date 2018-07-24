0d3f605dc52dbef3391109ccbcf17b5624ecc910
anntreesa <anntreesa@qburst.com>
2018-07-24 16:49:23 +0530

    first commit

---
+++ src/main/java/com/qburst/maven_base/App.java
@@ -0,0 +1,17 @@
+package com.qburst.maven_base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void first()
+    {
+        System.out.println( "This line is from my first program!" );
+    }
+    public static int findSum(int a, int b) {
+    	int c = a + b;
+    	return c;
+    }
+}
