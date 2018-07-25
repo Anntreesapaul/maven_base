96c744c0fe1bc6c72398178d870b01e5ff2c1e2b
Anntreesa Paul <40013721+Anntreesapaul@users.noreply.github.com>
2018-07-25 11:33:13 +0530

    Update App.java

--- src/main/java/com/qburst/maven_base/App.java
+++ src/main/java/com/qburst/maven_base/App.java
@@ -1,17 +1,9 @@
 package com.qburst.maven_base;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void first()
     {
         System.out.println( "This line is from my first program!" );
-    }
-    public static int findSum(int a, int b) {
-    	int c = a + b;
-    	return c;
     }
 }
